62d93d8a53e9823d9de48f2fa1772de3acc53ec5
Heyter <12237569+Heyter@users.noreply.github.com>
2023-02-18 01:35:56 +0300

    sprite-compress.png

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import autoprefixer from 'autoprefixer'
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	base: "/js-famfamfam-search/",
-	server: {
-		port: 3000,
-	},
 	css: {
 		postcss: {
 			plugins: [ autoprefixer({}) ],
